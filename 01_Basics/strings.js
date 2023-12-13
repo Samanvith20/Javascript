e30423d1532f9e7e6162b17cec4bdac76ccc225f
Samanvith <Samanvith2005@gmail.com>
2023-12-13 16:50:21 +0530

    Added Strings

---
+++ 01_Basics/strings.js
@@ -0,0 +1,22 @@
+// declaration of strings
+const myname ="Samanvith Reddy"
+console.log(myname);
+// methods in strings:->
+// console.log(myname.length)-->15
+// console.log(myname.toUpperCase())-->SAMANVITH REDDY
+// console.log(myname.toLowerCase())-->samanvith reddy
+// console.log(myname.substring(0,8))-->Samanvit
+//console.log(myname.split(''))-->["S", "a", "m", "a", "n", "v", "i", "t", "h", " ", "R", "e", "d", "d", "y"]
+ //console.log(myname.charAt(3))-->a
+// console.log(myname.indexOf('v'))-->5
+//console.log(myname[3])-->a
+//console.log(myname.slice(-10,-3))-->vith Re
+// console.log(myname.includes('Reddy'))//-->true
+// console.log(myname.includes('reddy'))//-->false
+// console.log(myname.endsWith('Reddy'))//-->true
+const  myname2="     Samanvith      "
+// console.log(myname2)-->"     Samanvith      "
+// console.log(myname2.trim())//-->Samanvith
+const url="https://www.google.com"
+//console.log(url.replace('https','http'))-->http://www.google.com
+//console.log(myname.split(' '))->  ["Samanvith", "Reddy"]
