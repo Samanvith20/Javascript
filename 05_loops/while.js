d2e0723150ed238b287bfb2adc10fdd19253caca
Samanvith <Samanvith2005@gmail.com>
2023-12-15 18:44:15 +0530

    while and do whileloop

---
+++ 05_loops/while.js
@@ -0,0 +1,18 @@
+// while loop
+// it is used to iterate over a block of code a number of times
+// syntax
+//  while(condition){}
+//  ++++++++++++++Example+++++++++++
+let i=0;
+while(i<10){
+    console.log(i);
+    i++;
+}
+// do while loop
+ // first it will execute the code then it will check the condition
+  i=5
+  do{
+        console.log(i);
+        i++;
+  }
+    while(i<10)
