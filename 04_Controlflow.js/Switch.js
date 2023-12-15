f080fccf3adb015b5c6780c55ac0ec9d61830e0a
Samanvith <Samanvith2005@gmail.com>
2023-12-15 17:53:23 +0530

    Added controlFlow stmts

---
+++ 04_Controlflow.js/Switch.js
@@ -0,0 +1,18 @@
+//Switch
+//Switch is a control flow statement that allows a value to change control flow.
+//++++++++++++++++++++++++Example+++++++++++++++++++++++++
+ let day="wedenseday"
+  switch (day) {
+    case "monday":
+        console.log("Today is Monday");
+        break;
+    case "tuesday":
+        console.log("Today is Tuesday");
+        break;
+    case "wedenseday":
+        console.log("Today is Wedenseday");
+        break;
+    default:
+         console.log("Today is not a Sunday");
+        break;
+  }
---
+++ 04_Controlflow.js/if_else.js
@@ -0,0 +1,41 @@
+//if
+// it is used to check the condition is true or false
+const cricketername="Sachin";
+if(cricketername==="Sachin"){
+    //console.log("He is the god of cricket");
+}
+//if else
+// it is used to check the condition if condition is true then it will execute if block otherwise else block
+const cricketername1="Sachin";
+// if(cricketername1==="Sachin"){
+//     console.log("He is the god of cricket");
+// }
+// else{
+//     console.log("He is not the god of cricket");
+// }
+  // nested if else
+    // const cricketscore=240
+    // if(cricketscore>=200){
+    //     console.log("score is greater than 200");
+    // }
+    // else if(cricketscore>=150)
+    // {
+    //     console.log("score is greater than 150");
+    // }
+    // else
+    // {
+    //     console.log("score is less than 150");
+    // }
+     //using AND(&&) operator
+     // it will Excute when both condition is true
+      const crickrtteam="India";
+      const playername="Sachin";
+      const playername1="Virat";
+    //    if(crickrtteam==="India" && playername==="Sachin"){
+    //        console.log("He is the player of Indian team");
+    //    }
+        //using OR(||) operator
+        // it will Excute when any one condition is true
+        if(crickrtteam==="India" || playername1==="Dhoni"){
+            console.log("He is the player of Indian team");
+        }
